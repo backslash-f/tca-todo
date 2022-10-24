9011451cccd9d6084890da27208363bcbb79eed6
Fernando Fernandes <mfernandes.fernando@gmail.com>
2022-10-24 09:41:52 +0200

    Create Xcode project

---
+++ Todos/TodosApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodosApp.swift
+//  Todos
+//
+//  Created by Fernando Fernandes on 24.10.22.
+//
+
+import SwiftUI
+
+@main
+struct TodosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
