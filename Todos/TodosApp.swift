7b4b2bf86e8088feae643817b811d4e651fb62ef
Fernando Fernandes <mfernandes.fernando@gmail.com>
2022-10-24 10:49:32 +0200

    Finish part 1 of 4

--- Todos/TodosApp.swift
+++ Todos/TodosApp.swift
@@ -5,13 +5,38 @@
 //  Created by Fernando Fernandes on 24.10.22.
 //
 
+import ComposableArchitecture
 import SwiftUI
 
 @main
 struct TodosApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            ContentView(
+                store: Store(
+                    initialState: AppState(
+                        todos: [
+                            Todo(
+                                description: "Milk",
+                                id: UUID(),
+                                isComplete: false
+                            ),
+                            Todo(
+                                description: "Eggs",
+                                id: UUID(),
+                                isComplete: false
+                            ),
+                            Todo(
+                                description: "Hand Soap",
+                                id: UUID(),
+                                isComplete: true
+                            )
+                        ]
+                    ),
+                    reducer: appReducer,
+                    environment: AppEnvironment()
+                )
+            )
         }
     }
 }
